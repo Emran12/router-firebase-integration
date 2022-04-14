a58841e68c263867444fc43190b0fc7cef1405ec
Emran12 <emran6452@gmail.com>
2022-04-14 08:37:17 +0600

    require auth added in Orders

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from "./components/Header/Header";
 import Register from "./components/Register/Register";
 import Products from "./components/Products/Products";
 import Orders from "./components/Orders/Orders";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 
 function App() {
   return (
@@ -15,7 +16,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/products" element={<Products></Products>}></Route>
-        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders></Orders>
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,15 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
-import useFirebase from "../../hooks/useFirebase";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+const auth = getAuth(app);
 
 const Login = () => {
-  const { signInWithGoogle } = useFirebase();
+  const [signInWithGoogle, user] = useSignInWithGoogle(auth);
   return (
     <div>
       <h1>Login </h1>
-      <button onClick={signInWithGoogle}>google sign in</button>
+      <button onClick={() => signInWithGoogle()}>google sign in</button>
       <form>
         <input type="email" placeholder="Enter your email" />
         <br />
